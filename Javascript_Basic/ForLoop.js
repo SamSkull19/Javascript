4d0a4013e1d3a9afad8ab03f14df9367a4d3bc0b
sifat-samin <sifatsamin19@gmail.com>
2025-09-19 23:31:51 +0600

    For Loop

---
+++ Javascript_Basic/ForLoop.js
@@ -0,0 +1,33 @@
+// Basic Counting
+for (var i = 1; i <= 5; i++) {
+    console.log("Count:", i);
+}
+
+// Print Even Numbers (2–10)
+for (var num = 2; num <= 10; num += 2) {
+    console.log("Even:", num);
+}
+
+// Print Odd Numbers (1–9)
+for (var odd = 1; odd < 10; odd += 2) {
+    console.log("Odd:", odd);
+}
+
+// Countdown
+for (var c = 5; c >= 1; c--) {
+    console.log("Countdown:", c);
+}
+console.log("Blast off 🚀");
+
+// Sum of First 5 Numbers
+var sum = 0;
+for (var n = 1; n <= 5; n++) {
+    sum += n;
+}
+console.log("Sum =", sum);
+
+// Loop through Array
+var colors = ["Red", "Green", "Blue"];
+for (var i = 0; i < colors.length; i++) {
+    console.log("Color:", colors[i]);
+}
