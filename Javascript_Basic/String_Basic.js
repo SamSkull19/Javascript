a0f2791aadb78d0208d408fa16815e3628f8b978
sifat-samin <sifatsamin19@gmail.com>
2025-09-20 15:43:33 +0600

    String Basic

---
+++ Javascript_Basic/String_Basic.js
@@ -0,0 +1,33 @@
+// Using single quotes
+let str1 = 'Hello World';
+console.log(str1);
+
+// Using double quotes
+let str2 = "JavaScript String";
+console.log(str2);
+
+// Using backticks (template literals)
+let str3 = `My name is Sifat`;
+console.log(str3);
+
+
+// String Example
+let myString = "Hello";
+console.log(myString[0]);  // H
+console.log(myString.length); // 5
+
+
+// Strings are immutable:
+myString[0] = "Y"; 
+console.log(myString); // Still "Hello"
+
+
+// Array Example
+let myArray = ["H", "e", "l", "l", "o"];
+console.log(myArray[0]);  // H
+console.log(myArray.length); // 5
+
+
+// Arrays are mutable:
+myArray[0] = "Y";
+console.log(myArray); // ["Y", "e", "l", "l", "o"]
