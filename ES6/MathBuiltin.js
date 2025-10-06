d0e16a26850192dcfc65a252c57a5a4a90b7d509
sifat-samin <sifatsamin19@gmail.com>
2025-10-07 00:00:12 +0600

    Math BuiltIn

---
+++ ES6/MathBuiltin.js
@@ -0,0 +1,7 @@
+console.log(Math.PI);       // 3.141592653589793
+console.log(Math.sqrt(16)); // 4
+console.log(Math.pow(2, 3));// 8
+console.log(Math.abs(-5));  // 5
+console.log(Math.random());  // random number between 0 and 1
+console.log(Math.floor(4.7)); // 4
+console.log(Math.ceil(4.3));  // 5
