7b2cbd717d8c816b4b6b38179c3b9f881f47cfb3
sifat-samin <sifatsamin19@gmail.com>
2025-10-06 19:11:46 +0600

    Async and Await Added Another Fetch

--- ES6/AsyncAwait.js
+++ ES6/AsyncAwait.js
@@ -45,6 +45,15 @@ const dataLoadTryCatch = async () => {
 dataLoadTryCatch();
 
 
+async function fetchUsers(){
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    console.log(data);
+}
+
+fetchUsers();
+console.log("This runs before fetch completes");
+
 /*
 Summary:
 
